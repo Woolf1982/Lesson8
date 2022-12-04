381a9e2205b35c1c6d1ef868204799f1c9871621
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-12-04 09:47:57 +0300

    Ex054

--- Ex054/Program.cs
+++ Ex054/Program.cs
@@ -10,3 +10,64 @@
 8 4 4 2
 */
 
+Console.WriteLine($"Enter array size (m x n): ");
+int m = InputNumbers("Enter m: ");
+int n = InputNumbers("Enter n: ");
+int range = InputNumbers("Enter Range: from 1 to ");
+
+int[,] matrix = new int[m, n];
+CreateMatrix(matrix);
+WriteMatrix(matrix);
+
+Console.WriteLine($"Sorted array: ");
+OrderMatrixLines(matrix);
+WriteMatrix(matrix);
+
+void OrderMatrixLines(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
